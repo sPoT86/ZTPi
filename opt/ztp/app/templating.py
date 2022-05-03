587344555231866bfe0cbbac76a79f6c506c25fe
sPoT <104821043+sPoT86@users.noreply.github.com>
2022-05-03 16:46:25 +0200

    Update templating.py

--- opt/ztp/app/templating.py
+++ opt/ztp/app/templating.py
@@ -1,4 +1,4 @@
-from jinja2 import Environment, FileSystemLoader, StrictUndefined
+from jinja2 import Environment, FileSystemLoader, StrictUndefined, TemplateNotFound, UndefinedError, TemplateSyntaxError
 from app import configuration as C
 import time
 import os
@@ -6,8 +6,8 @@ import os
 
 def render_myfile(template, parameter):
 
-    if not os.path.isfile(C.TEMPLATE_DIR + template):
-        return('T68')
+    if not os.path.isdir(C.TEMPLATE_DIR):
+        return('T74')
     else:
         env = Environment(
             loader=FileSystemLoader(C.TEMPLATE_DIR),
@@ -15,8 +15,18 @@ def render_myfile(template, parameter):
             trim_blocks=True
         )
 
-        template = env.get_template(template)
-        return template.render(parameter)
+        try:
+            template = env.get_template(template)
+            try:
+                config = template.render(parameter)
+            except UndefinedError:
+                return('T81')
+            except TemplateSyntaxError:
+                return('T82')
+            else:
+                return template.render(parameter)
+        except TemplateNotFound:
+            return('T68')
 
 
 def render_file(template, **kwargs):
