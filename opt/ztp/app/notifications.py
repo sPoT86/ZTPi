e4b521c0ef7490181bd441a31b71c80c00fd1602
sPoT <104821043+sPoT86@users.noreply.github.com>
2022-07-11 15:46:30 +0200

    Add files via upload

--- opt/ztp/app/notifications.py
+++ opt/ztp/app/notifications.py
@@ -1,25 +1,37 @@
 import json
 import os
+import requests
 from discord_webhook import DiscordWebhook
 from app import configuration as C
 
 
-headers = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'User-Agent': 'ZTPi'
-}
-
-
-def notify_discord(msg):
-    url = <Add WebHook URL as string>
+def notify_im(msg):
+    # Discord
+    url = 'https://discordapp.com/api/webhooks/764566548975190026/V1Mn2Bq9et-WsKrFuJHZ35yy-lpDBrhB3IWtE9vI2pxNSJyxXU0TVGX0uSCzSS2aMiea'
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'User-Agent': 'ZTPi'
+        }
     webhook = DiscordWebhook(url, content=json.dumps(msg))
     response = webhook.execute()
+    # WebEx
+#    url = 'https://webexapis.com/v1/webhooks/incoming/Y2lzY29zcGFyazovL3VybjpURUFNOmV1LWNlbnRyYWwtMV9rL1dFQkhPT0svYmUzMDI3OTMtZmMyNS00MTc5LTg0OGMtMGU3ZTEyOGIwN2Y5'
+#    header = {
+#        'Content-Type': 'application/json'
+#        }
+#    data = {
+#        'text': msg
+#        }
+#    response = requests.post(url,headers=header,data=json.dumps(data))
+    # Without IM
+    pass
+
 
 
 def notify_syslog(msg):
     print(msg)
-    with open(os.path.join(C.CACHE_DIR, C.LOG_FILENAME), "a+") as fh:
+    with open(os.path.join(C.LOG_DIR, C.LOG_FILENAME), "a+") as fh:
         fh.write("\n") 
         fh.write(msg)
     os.chmod(C.CACHE_DIR + C.LOG_FILENAME, 0o777)
--- opt/ztp/app/tasks.py
+++ opt/ztp/app/tasks.py
@@ -1,6 +1,6 @@
 from app.network import get_napalm_connection
 from app.templating import render_myfile
-from app.notifications import notify_discord
+from app.notifications import notify_im
 from app.notifications import notify_syslog
 from app.inventory import parameter_lookup
 from app import configuration as C
@@ -14,49 +14,49 @@ def ztp_start(host, file):
         dtime = datetime.datetime.now()
         msg = '{} downloaded {}'.format(host, file)
         notify_syslog('{}: {}'.format(dtime, msg))
-        notify_discord(msg)
+        notify_im(msg)
 
         dev = get_napalm_connection(host, 'ios')
 
         if dev:
             msg = '{} connection established'.format(host)
             notify_syslog('{}: {}'.format(dtime, msg))
-            notify_discord(msg)
+            notify_im(msg)
         else:
             msg = '{} connection failed, giving up'.format(host)
             notify_syslog('{}: {}'.format(dtime, msg))
-            notify_discord(msg)
+            notify_im(msg)
             return
 
         facts = dev.get_facts()
         msg = '{} {}/{}/{}'.format(host, facts['hostname'], facts['model'], facts['serial_number'])
         notify_syslog('{}: {}'.format(dtime, msg))
-        notify_discord(msg)
+        notify_im(msg)
         ztplog = str(facts['hostname'] + ".log")
         parameters = parameter_lookup(facts['serial_number'])
         with open(os.path.join(C.CACHE_DIR, ztplog), "w") as fh:
             if parameters == 'K22':
                 msg = '{} no Lookup possible, Keystore is missing'.format(host)
                 notify_syslog('{}: {}'.format(dtime, msg))
-                notify_discord(msg)
+                notify_im(msg)
                 logentry = '{};{};{};{};{};{};'.format(dtime, host, facts['hostname'], facts['model'], facts['serial_number'], 'KFAILURE')
                 fh.write(logentry)
             elif parameters is None:
                 msg = '{} Serial {} not assigned in Keystore'.format(host, facts['serial_number'])
                 notify_syslog('{}: {}'.format(dtime, msg))
-                notify_discord(msg)
+                notify_im(msg)
                 logentry = '{};{};{};{};{};{};'.format(dtime, host, facts['hostname'], facts['model'], facts['serial_number'], 'UNKNOWN')
                 fh.write(logentry)
             else:
                 devicename = parameters[0]['devicename']
                 msg = '{} Serial {} assigned in Keystore -> Realname: {}'.format(host, facts['serial_number'], devicename)
                 notify_syslog('{}: {}'.format(dtime, msg))
-                notify_discord(msg)
+                notify_im(msg)
                 if os.path.isfile(C.CONFIG_DIR + devicename):
                     with open(os.path.join(C.CONFIG_DIR, devicename), "r") as f:
                         msg = '{} existing backup configuration for {} found'.format(host, devicename)
                         notify_syslog('{}: {}'.format(dtime, msg))
-                        notify_discord(msg)
+                        notify_im(msg)
                         logentry = '{};{};{};{};{};{};'.format(dtime, host, facts['hostname'], facts['model'], facts['serial_number'], devicename)
                         config = f.read()
                         fh.write(logentry)
@@ -67,32 +67,32 @@ def ztp_start(host, file):
                 else:
                     msg = '{} no backup configuration for {} found'.format(host, devicename)
                     notify_syslog('{}: {}'.format(dtime, msg))
-                    notify_discord(msg)
+                    notify_im(msg)
                     config = render_myfile(parameters[0]['association'], parameters[0])
                     if config == 'T74':
                         logentry = '{};{};{};{};{};{};'.format(dtime, host, facts['hostname'], facts['model'], facts['serial_number'], 'TFAILURE')
                         fh.write(logentry)
                         msg = '{} no Rendering possible, Template folder is missing or not accessible'.format(host)
                         notify_syslog('{}: {}'.format(dtime, msg))
-                        notify_discord(msg)
+                        notify_im(msg)
                     elif config == 'T68':
                         logentry = '{};{};{};{};{};{};'.format(dtime, host, facts['hostname'], facts['model'], facts['serial_number'], 'TFAILURE')
                         fh.write(logentry)
                         msg = '{} no Rendering possible, Template {} is missing'.format(host, parameters[0]['association'])
                         notify_syslog('{}: {}'.format(dtime, msg))
-                        notify_discord(msg)
+                        notify_im(msg)
                     elif config == 'T81':
                         logentry = '{};{};{};{};{};{};'.format(dtime, host, facts['hostname'], facts['model'], facts['serial_number'], 'TFAILURE')
                         fh.write(logentry)
                         msg = '{} no Rendering possible, Template {} includes not defined variables'.format(host, parameters[0]['association'])
                         notify_syslog('{}: {}'.format(dtime, msg))
-                        notify_discord(msg)
+                        notify_im(msg)
                     elif config == 'T82':
                         logentry = '{};{};{};{};{};{};'.format(dtime, host, facts['hostname'], facts['model'], facts['serial_number'], 'TFAILURE')
                         fh.write(logentry)
                         msg = '{} no Rendering possible, Template {} has errors in Syntax'.format(host, parameters[0]['association'])
                         notify_syslog('{}: {}'.format(dtime, msg))
-                        notify_discord(msg)
+                        notify_im(msg)
                     else:
                         logentry = '{};{};{};{};{};{};'.format(dtime, host, facts['hostname'], facts['model'], facts['serial_number'], devicename)
                         fh.write(logentry)
@@ -105,7 +105,7 @@ def ztp_start(host, file):
         dtime = datetime.datetime.now()
         msg = '{} downloaded {}'.format(host, file)
         notify_syslog('{}: {}'.format(dtime, msg))
-        notify_discord(msg)
+        notify_im(msg)
         ztpname = file.split('-')[0]
         ztplog = str(ztpname + ".log")
 
@@ -116,11 +116,11 @@ def ztp_start(host, file):
         if dev:
             msg = '{} connection established'.format(host)
             notify_syslog('{}: {}'.format(dtime, msg))
-            notify_discord(msg)
+            notify_im(msg)
         else:
             msg = '{} connection failed, giving up'.format(host)
             notify_syslog('{}: {}'.format(dtime, msg))
-            notify_discord(msg)
+            notify_im(msg)
             return
 
         with open(os.path.join(C.CACHE_DIR, ztplog), "r") as f:
@@ -129,17 +129,17 @@ def ztp_start(host, file):
             if devicename == 'UNKNOWN':
                 msg = '{} Serial not assigned in Keystore, please verify and restart device manually'.format(host)
                 notify_syslog('{}: {}'.format(dtime, msg))
-                notify_discord(msg)
+                notify_im(msg)
                 return
             elif devicename == 'KFAILURE':
                 msg = '{} Provisioning failed, device will restart in 5 minutes'.format(host)
                 notify_syslog('{}: {}'.format(dtime, msg))
-                notify_discord(msg)
+                notify_im(msg)
                 return
             elif devicename == 'TFAILURE':
                 msg = '{} Provisioning failed, device will restart in 5 minutes'.format(host)
                 notify_syslog('{}: {}'.format(dtime, msg))
-                notify_discord(msg)
+                notify_im(msg)
                 return
 
         facts = dev.get_facts()
@@ -148,11 +148,11 @@ def ztp_start(host, file):
         if facts['hostname'] == devicename:
             msg = '{} {} successfully provisioned, device will start embedded scripts in about 100s.'.format(host, facts['hostname'])
             notify_syslog('{}: {}'.format(dtime, msg))
-            notify_discord(msg)
+            notify_im(msg)
         else:
             msg = '{} something\'s wrong with me'.format(host)
             notify_syslog('{}: {}'.format(dtime, msg))
-            notify_discord(msg)
+            notify_im(msg)
 
     dev.close()
 
