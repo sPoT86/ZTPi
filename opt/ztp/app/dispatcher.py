78f987b11cc96e381432f8f7e28b9af6d5d5bf6c
sPoT <104821043+sPoT86@users.noreply.github.com>
2023-03-04 14:44:22 +0100

    Update dispatcher.py

--- opt/ztp/app/dispatcher.py
+++ opt/ztp/app/dispatcher.py
@@ -26,36 +26,57 @@ class TftpData:
 
 def request_dispatcher(file_path):
 
-    if "ZTP" in file_path:
-        ztpname = file_path.split('-')[0]
-        ztplog = str(ztpname + ".log")
-        while os.path.isfile(C.CACHE_DIR + ztplog) is False:
-            time.sleep(1)
-        with open(os.path.join(C.CACHE_DIR, ztplog), "r") as f:
-            cache = f.readline().split(';')
-            serial = cache[4]
-            devicename = cache[5]
-            if devicename == 'UNKNOWN':
-                config = render_file('unknownhost-confg', staging_enasec=C.STAGING_ENASEC)
-            elif devicename == 'DFAILURE':
-                config = render_file('failedhost-confg')
-            elif devicename == 'TFAILURE':
-                config = render_file('failedhost-confg')
-            elif os.path.isfile(C.CONFIG_DIR + devicename):
-                with open(os.path.join(C.CONFIG_DIR, devicename), "r") as fh:
-                    config = fh.read()
-            else:
-                parameters = parameter_lookup(serial)
-                config = render_file(parameters['ztp_template'], **parameters)
-            if config not in ['T68','T74','T81','T82']:
-                return StringResponseData(config)
-
     if file_path == 'network-confg':
         tname = generate_tname()
         config = render_file('network-confg', hostname=tname, staging_bn=C.STAGING_BN, staging_pw=C.STAGING_PW, staging_domain=C.STAGING_DOMAIN)
-        if config not in ['T68','T74','T81','T82']:
+        if config not in ['','T68','T74','T81','T82']:
+            return StringResponseData(config)
+
+    elif "ZTP" in file_path:
+        ztpname = file_path.split('-')[0]
+        cachefile = str(ztpname + ".log")
+        timeout = 10
+        i = 1
+        while i != timeout and os.path.isfile(C.CACHE_DIR + cachefile) is False:
+            time.sleep(1)
+            i += 1
+        if i == timeout:
+            config = render_file('failedhost-confg')
+        else:
+            with open(os.path.join(C.CACHE_DIR, cachefile), "r") as f:
+                cache = f.readline().split(';')
+                i = 1
+                while i != timeout and len(cache) < 6:
+                    time.sleep(1)
+                    cache = f.readline().split(';')
+                    i += 1
+                if i == timeout:
+                    devicename = 'CFAILURE'
+                else:
+                    devicename = cache[5]
+                if devicename == 'CFAILURE':
+                    config = render_file('failedhost-confg')
+                elif devicename == 'DFAILURE':
+                    config = render_file('failedhost-confg')
+                elif devicename == 'TFAILURE':
+                    config = render_file('failedhost-confg')
+                elif devicename == 'UNKNOWN':
+                    config = render_file('unknownhost-confg', staging_enasec=C.STAGING_ENASEC)
+                else:
+                    cache = f.read().split(';')
+                    i = 1
+                    while i != timeout and len(cache) < 7:
+                        time.sleep(1)
+                        cache = f.read().split(';')
+                        i += 1
+                    if i == timeout:
+                        config = render_file('failedhost-confg')
+                    else:
+                        config = cache[6]
+        if config not in ['','T68','T74','T81','T82']:
             return StringResponseData(config)
 
     else:
-        return TftpData(file_path)
+        if os.path.isfile(C.TFTP_ROOT, file_path):
+            return TftpData(file_path)
 
