7394b9e8c328972d52465a34cacebb906419c128
sPoT <104821043+sPoT86@users.noreply.github.com>
2023-03-07 18:30:31 +0100

    Update notifications.py

--- opt/ztp/app/notifications.py
+++ opt/ztp/app/notifications.py
@@ -6,24 +6,27 @@ from app import configuration as C
 
 
 def notify_im(msg):
-    # Discord
-#    url = 'https://discordapp.com/api/webhooks/[URL]'
-#    headers = {
-#        'Content-Type': 'application/json',
-#        'Accept': 'application/json',
-#        'User-Agent': 'ZTPi'
-#        }
-#    webhook = DiscordWebhook(url, content=json.dumps(msg))
-#    response = webhook.execute()
-    # WebEx
-#    url = 'https://webexapis.com/v1/webhooks/incoming/[URL]'
-#    header = {
-#        'Content-Type': 'application/json'
-#        }
-#    data = {
-#        'text': msg
-#        }
-#    response = requests.post(url,headers=header,data=json.dumps(data))
+    try:
+        # Discord
+        url = 'https://discordapp.com/api/webhooks/764566548975190026/V1Mn2Bq9et-WsKrFuJHZ35yy-lpDBrhB3IWtE9vI2pxNSJyxXU0TVGX0uSCzSS2aMiea'
+        headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'User-Agent': 'ZTPi'
+            }
+        webhook = DiscordWebhook(url, content=json.dumps(msg))
+        response = webhook.execute()
+        # WebEx
+#        url = 'https://webexapis.com/v1/webhooks/incoming/Y2lzY29zcGFyazovL3VybjpURUFNOmV1LWNlbnRyYWwtMV9rL1dFQkhPT0svYmUzMDI3OTMtZmMyNS00MTc5LTg0OGMtMGU3ZTEyOGIwN2Y5'
+#        header = {
+#            'Content-Type': 'application/json'
+#            }
+#        data = {
+#            'text': msg
+#            }
+#        response = requests.post(url,headers=header,data=json.dumps(data))
+    except Exception:
+        pass
     # Without IM
     pass
 
