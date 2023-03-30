34592d05adfe21ee6269506f812d632907c62eda
sPoT <104821043+sPoT86@users.noreply.github.com>
2023-03-30 15:34:31 +0200

    Update notifications.py

--- opt/ztp/app/notifications.py
+++ opt/ztp/app/notifications.py
@@ -1,6 +1,7 @@
 import json
 import os
 import requests
+import datetime
 from app import configuration as C
 
 
@@ -22,14 +23,27 @@ def notify_im(msg):
 #            'text': msg
 #            }
 #        response = requests.post(url,headers=header,data=json.dumps(data))
-#    except Exception:
-#        pass
+    # IM error Handling
+#    except requests.exceptions.ConnectionError as e:
+#        notify_syslog(f"IM - network error: {e}")
+#    else:
+#        if response.status_code == 200:
+#            pass
+#        elif response.status_code == 401:
+#            notify_syslog("IM error - wrong login data")
+#        elif response.status_code == 403:
+#            notify_syslog("IM error - not permitted")
+#        elif response.status_code == 404:
+#            notify_syslog("IM error - ressource not found")
+#        elif response.status_code == 408:
+#            notify_syslog("IM error - timeout")
     # Without IM
     pass
 
 
 def notify_syslog(msg):
-    with open(os.path.join(C.LOG_DIR, C.LOG_FILENAME), "a+") as fh:
-        fh.write("\n") 
-        fh.write(msg)
+    dtime = datetime.datetime.now()
+    with open(os.path.join(C.LOG_DIR, C.LOG_FILENAME), 'a+') as fh:
+        fh.write("\n")
+        fh.write(f"{dtime}: {msg}")
     os.chmod(C.LOG_DIR + C.LOG_FILENAME, 0o777)
