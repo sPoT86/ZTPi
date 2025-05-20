99abfc7e97aa90193fa748b6de11b41f2bcc5bef
sPoT <104821043+sPoT86@users.noreply.github.com>
2025-05-20 11:36:39 +0200

    Update network.py

--- opt/ztp/app/network.py
+++ opt/ztp/app/network.py
@@ -2,11 +2,10 @@ from napalm import get_network_driver
 from app import configuration as C
 import time
 
-def get_napalm_connection(host, device_type, attempts=120, timeout=1):
+def get_napalm_connection(host, device_type, attempts=25, timeout=1):
     driver = get_network_driver(device_type)
     device = driver(hostname=host, username=C.STAGING_BN,
                     password=C.STAGING_PW)
-
     for _ in range(attempts):
         try:
             device.open()
